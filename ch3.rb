8ad208b3b111152477ef099e848c8dde5f6ed58d
Q4living <thomasqian@yahoo.com>
2016-04-05 22:52:09 +0800

    My answer to this challenge

---
+++ ch3.rb
@@ -0,0 +1,24 @@
+# Standard ruby library for XML parsing
+require 'rexml/document'
+include REXML
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+xmlText = "" 
+
+# Read the input XML Fragment
+while line = gets()
+   xmlText += line
+end
+
+doc = Document.new xmlText
+# XPath Selector for listing the format of the movies with popularity<8, in the same order as which they occur in the given XML.
+# Fill in the blanks to complete the required XPath selector query
+x = ""
+
+# My Code below
+doc.elements.each("collection/movie/format"){ 
+    |e| x != e.get_text.value ? (
+           puts e.get_text.value
+           x = e.get_text.value) : ()
+    }
+# End of my code
